30925df2bc40c3ade1651d641ea51a4da0a1c9ac
SebasTamayoA <sebastamayo37@gmail.com>
2022-08-07 17:33:00 -0500

    Actualizacion de reportesView

--- src/main/java/com/mycompany/view/ReportesView.java
+++ src/main/java/com/mycompany/view/ReportesView.java
@@ -32,7 +32,11 @@ public class ReportesView extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String titulo = " Información respectiva al “Líder”.";
-                panelResultados(titulo);
+                try {
+                    panelResultados(titulo, reportesController.listarInfoLider());
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
             });
         btnInforme2.addActionListener(new ActionListener() {
@@ -41,14 +45,22 @@ public class ReportesView extends JFrame {
                 String titulo = "Información de los proyectos cuya clasificación sea \n" +
                         "“Casa Campestre” y que estén ubicados en las ciudades de “Santa Marta”, “Cartagena” y\n" +
                         "“Barranquilla”";
-                panelResultados(titulo);
+                try {
+                    panelResultados(titulo, reportesController.listarInfoProyecto());
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
         btnInforme3.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String titulo = " Compras realizadas por los proyectos con el proveedor “Homecenter” y para la ciudad “Salento”";
-                panelResultados(titulo);
+                try {
+                    panelResultados(titulo, reportesController.listarComprasProyecto());
+                } catch (SQLException ex) {
+                    throw new RuntimeException(ex);
+                }
             }
         });
         btnSalir.addActionListener(new ActionListener() {
@@ -60,36 +72,32 @@ public class ReportesView extends JFrame {
         });
     }
 
-    // metodo para mostrar los resultados de la consulta en el panel
-    public void panelResultados(String titulo) {
-        try {
-            // limpiar el Jpanel
-            panel1.removeAll();
-            // mostrar el informe 3 en el jTable tblResultados y agregar un scrollPanel al jFrame
-            DefaultTableModel model = reportesController.listarComprasProyecto();
-            tblResultados.setModel(model);
-            JScrollPane scrollPane = new JScrollPane(tblResultados);
-            panel1.setLayout(new BorderLayout());
-            panel1.add(scrollPane, BorderLayout.CENTER);
-            // poner botones al final del jFrame para volver a la ventana principal y salir
-            JPanel panel2 = new JPanel();
-            panel2.add(btnInforme1);
-            panel2.add(btnInforme2);
-            panel2.add(btnInforme3);
-            panel2.add(btnSalir);
-            panel1.add(panel2, BorderLayout.SOUTH);
-            // agregar nuevo label en la parte superior y centrarlo
-            JLabel lblTitulo2 = new JLabel(titulo);
-            panel1.add(lblTitulo2, BorderLayout.NORTH);
-            lblTitulo2.setHorizontalAlignment(lblTitulo2.CENTER);
-            // agragar color #E7F6F2 a la letra del label
-            lblTitulo2.setForeground(new Color(231, 246, 242));
-            // agregar margenes al label
-            lblTitulo2.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
-            pack(); // ajustar el tamaño del jFrame a los componentes que contiene
 
-        } catch (SQLException ex) {
-            JOptionPane.showMessageDialog(null, "Error al listar los resultados");
-        }
+    public void panelResultados(String titulo, DefaultTableModel model) {
+        // limpiar el Jpanel
+        panel1.removeAll();
+        // agregar DefaultTableModel a la tabla
+        tblResultados.setModel(model);
+        // mostrar el informe 3 en el jTable tblResultados y agregar un scrollPanel al jFrame
+        JScrollPane scrollPane = new JScrollPane(tblResultados);
+        panel1.setLayout(new BorderLayout());
+        panel1.add(scrollPane, BorderLayout.CENTER);
+        // poner botones al final del jFrame para volver a la ventana principal y salir
+        JPanel panel2 = new JPanel();
+        panel2.add(btnInforme1);
+        panel2.add(btnInforme2);
+        panel2.add(btnInforme3);
+        panel2.add(btnSalir);
+        panel1.add(panel2, BorderLayout.SOUTH);
+        // agregar nuevo label en la parte superior y centrarlo
+        JLabel lblTitulo2 = new JLabel(titulo);
+        panel1.add(lblTitulo2, BorderLayout.NORTH);
+        lblTitulo2.setHorizontalAlignment(lblTitulo2.CENTER);
+        // agregar color #E7F6F2 a la letra del label
+        lblTitulo2.setForeground(new Color(231, 246, 242));
+        // agregar margenes al label
+        lblTitulo2.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        pack(); // ajustar el tamaño del jFrame a los componentes que contiene
+
     }
 }
